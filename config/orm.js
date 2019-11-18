c660e8809a019c7e39065f0013b302d465184b24
Matt Wigdahl <matt@leverpulley.com>
2019-11-18 10:53:39 -0700

    setup baseline file structure.

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,54 @@
 //require connection export
+var connection = require("./connection.js");
 
 // create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
-// selectAll()
-// insertOne()
-// updateOne()
 
-module.export
+var orm = {
+    selectAll: function(whatToSelect, tableInput) {
+        var queryString = "SELECT ?? FROM ??"; 
+        connection.query(queryString, [whatToSelect, tableInput], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+            
+        });
+    },
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+              throw err;
+            }
+      
+            cb(result);
+          });
+    },
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
+    },
+
+
+module.exports = orm;
