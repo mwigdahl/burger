e37936833da264d5b649e6e2d84438794f516a33
Matt Wigdahl <matt@leverpulley.com>
2019-11-20 22:02:03 -0700

    cleaned up code for production

--- config/orm.js
+++ config/orm.js
@@ -18,14 +18,11 @@ function objToSql(ob) {
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
+
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
@@ -43,42 +40,41 @@ var orm = {
       }
       cb(result);
     });
-    },
-    insertOne: function(tableInput, cols, vals, cb) {
-        var queryString = "INSERT INTO " + tableInput;
+  },
+  insertOne: function(tableInput, cols, vals, cb) {
+    var queryString = "INSERT INTO " + tableInput;
 
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
-      
-        connection.query(queryString, vals, function(err, result) {
-            if (err) {
-              throw err;
-            }
-      
-            cb(result);
-          });
-    },
-    updateOne: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
-    
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
-        console.log('queryString', queryString);
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-    
-          cb(result);
-        });
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
       }
-    }
 
+      cb(result);
+    });
+  },
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+    console.log("queryString", queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
 
 module.exports = orm;
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,45 +1,16 @@
 $(function() {
-    $(".devour-it").on("click", function(event) {
-      //event.preventDefault();
-      //console.log("event", event);
-      
-      var id = $(this).data("id");
-      console.log("ID", id);
-        var isDevoured = {
-            devoured: 1
-        };
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: isDevoured
-      }).then(
-        function(result) {
-            console.log('result', result);
-          location.reload();
-        }
-      );
-  
-    });  
-  
-    // $(".create-form").on("submit", function(event) {
-    //   // Make sure to preventDefault on a submit event.
-    //   event.preventDefault();
-  
-    //   var newCat = {
-    //     name: $("#ca").val().trim(),
-    //     sleepy: $("[name=sleepy]:checked").val().trim()
-    //   };
-  
-    //   // Send the POST request.
-    //   $.ajax("/api/cats", {
-    //     type: "POST",
-    //     data: newCat
-    //   }).then(
-    //     function() {
-    //       console.log("created new cat");
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+  $(".devour-it").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var isDevoured = {
+      devoured: 1
+    };
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: isDevoured
+    }).then(function(result) {
+      location.reload();
+    });
   });
+});
